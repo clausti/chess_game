cf8330c2a928dd75a976ee3a7343cf88c2c407f9
App Academy Student <student@appacademy.io>
2013-09-25 17:58:31 -0700

    Implement HumanPlayer class, moved input methods

--- game_class.rb
+++ game_class.rb
@@ -5,28 +5,10 @@ require './chess_errors.rb'
 
 class ChessGame
 
-     ROW_NOTATION = { "8" => 0,
-                      "7" => 1,
-                      "6" => 2,
-                      "5" => 3,
-                      "4" => 4,
-                      "3" => 5,
-                      "2" => 6,
-                      "1" => 7 }
-
-
-  COLUMN_NOTATION = { "a" => 0,
-                      "b" => 1,
-                      "c" => 2,
-                      "d" => 3,
-                      "e" => 4,
-                      "f" => 5,
-                      "g" => 6,
-                      "h" => 7 }
-
   def initialize
     @board = Board.new
-    @active_player = :white
+    @players = [HumanPlayer.new(:white), HumanPlayer.new(:black)]
+    @active_player = @players.first
     play
   end
 
@@ -47,8 +29,8 @@ class ChessGame
   end
 
   def one_turn
-    puts "#{@active_player.to_s.capitalize}'s turn!"
-    puts "You're in check!" if @board.in_check?(@active_player)
+    puts "#{@active_player.color.to_s.capitalize}'s turn!"
+    puts "You're in check!" if @board.in_check?(@active_player.color)
     begin
       start_pos, end_pos = player_move
     rescue InvalidMoveError => err
@@ -58,6 +40,20 @@ class ChessGame
     @board.move_piece(start_pos, end_pos)
     @board.render
     change_players
+  end
+
+  def player_move
+    begin
+      start_pos, end_pos = @active_player.ask_coord_from_player
+    rescue InputError => err
+      puts err.message
+      retry
+    end
+    raise InvalidMoveError, "There's no piece at start location!" if @board.piece_at(start_pos).nil?
+    raise InvalidMoveError, "That's not your piece at start location!" unless @board.piece_at(start_pos).color == @active_player.color
+    valid_move = @board.valid_move_from_player?(start_pos, end_pos)
+
+    [start_pos, end_pos]
   end
 
   def checkmate
@@ -73,46 +69,8 @@ class ChessGame
     false
   end
 
-  def player_move
-    begin
-      start_pos, end_pos = ask_coord_from_player
-    rescue InputError => err
-      puts err.message
-      retry
-    end
-
-    valid_move = @board.valid_move_from_player?(start_pos, end_pos)
-
-    [start_pos, end_pos]
-  end
-
-  def ask_coord_from_player #should really be in HumanPlayer class.
-    print "Move from? (format: a7, etc)  "
-    start_pos = convert_player_input_to_coords(gets.chomp)
-
-    raise InputError, "There's no piece there!" if @board.piece_at(start_pos).nil?
-    raise InputError, "That's not your piece!" unless @board.piece_at(start_pos).color == @active_player
-    raise InputError, "Not valid start coordinates" unless (start_pos.is_a?(Array) && start_pos.length == 2)
-    raise InputError, "Not valid start coordinates" unless start_pos.all? {|ord| ord >= 0 && ord < 8}
-
-    print "Move to? (format: a7, etc)  "
-    end_pos = convert_player_input_to_coords(gets.chomp)
-    raise InputError, "Not valid end coordinates" unless (end_pos.is_a?(Array) && end_pos.length == 2)
-    raise InputError, "Not valid end coordinates" unless end_pos.all? {|ord| ord >= 0 && ord < 8}
-
-    [start_pos, end_pos]
-  end
-
-  def convert_player_input_to_coords(input)
-    input = input.downcase.reverse.split('')
-    row, column = input
-    row = ROW_NOTATION[row]
-    column = COLUMN_NOTATION[column]
-    [row, column]
-  end
-
   def change_players
-    @active_player = (@active_player == :white) ? :black : :white
+    @active_player = (@active_player == @players.first) ? @players.first : @players.last
   end
 
 end
--- player_classes.rb
+++ player_classes.rb
@@ -1,3 +1,55 @@
 class HumanPlayer
 
+     ROW_NOTATION = { "8" => 0,
+                      "7" => 1,
+                      "6" => 2,
+                      "5" => 3,
+                      "4" => 4,
+                      "3" => 5,
+                      "2" => 6,
+                      "1" => 7 }
+
+
+  COLUMN_NOTATION = { "a" => 0,
+                      "b" => 1,
+                      "c" => 2,
+                      "d" => 3,
+                      "e" => 4,
+                      "f" => 5,
+                      "g" => 6,
+                      "h" => 7 }
+
+  attr_accessor :color
+
+  def initialize(color)
+    @color = color
+  end
+
+  def ask_coord_from_player
+    print "Move from? (format: a7, etc)  "
+    start_pos = gets.chomp
+    raise InputError, "Not valid coordinates" if start_pos.length != 2
+    start_pos = convert_player_input_to_coords(start_pos)
+    raise InputError, "Not valid start coordinates" if start_pos.any? {|el| el.nil?}
+    raise InputError, "Not valid start coordinates" unless (start_pos.is_a?(Array) && start_pos.length == 2)
+    raise InputError, "Not valid start coordinates" unless start_pos.all? {|ord| ord >= 0 && ord < 8}
+
+    print "Move to? (format: a7, etc)  "
+    end_pos = gets.chomp
+    raise InputError, "Not valid coordinates" if end_pos.length != 2
+    end_pos = convert_player_input_to_coords(end_pos)
+    raise InputError, "Not valid end coordinates" if end_pos.any? {|el| el.nil?}
+    raise InputError, "Not valid end coordinates" unless (end_pos.is_a?(Array) && end_pos.length == 2)
+    raise InputError, "Not valid end coordinates" unless end_pos.all? {|ord| ord >= 0 && ord < 8}
+
+    [start_pos, end_pos]
+  end
+
+  def convert_player_input_to_coords(input)
+    input = input.downcase.reverse.split('')
+    row, column = input
+    row = ROW_NOTATION[row]
+    column = COLUMN_NOTATION[column]
+    [row, column]
+  end
 end
